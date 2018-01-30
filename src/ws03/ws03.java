432d9840723dd3cdeada3e381370d21a186b2cff
student <student@M11038>
2018-01-30 17:30:54 +0900

    .

---
+++ src/ws03/ws03.java
@@ -0,0 +1,22 @@
+package ws03;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ws03 {
+
+	public static void main(String[] args) {
+		System.out.println("1~9 중 숫자 3개를 입력하세요");
+		Scanner sc = new Scanner(System.in);
+		int arr[] = new int[3] ;
+		arr[0] = sc.nextInt();
+		arr[1] = sc.nextInt();
+		arr[2] = sc.nextInt();
+		Arrays.sort(arr);
+		System.out.println("최대값:"+arr[2]);
+		System.out.println("최소값:"+arr[0]);
+		System.out.println("차   : "+(arr[2]-arr[0]));
+		
+	}
+
+}
